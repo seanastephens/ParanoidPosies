1d01a312ec15b2b7cc989b0de57f583388b91795
kuba1 <kubastechly@gmail.com>
2014-04-04 21:06:07 -0700

    Added MockGame and MockThing classes for testing purposes.

---
+++ ParanoidPosies/src/GUI/MockThing.java
@@ -0,0 +1,75 @@
+package GUI;
+
+import java.awt.Image;
+import java.awt.Point;
+
+import model.Thing;
+
+public class MockThing implements Thing {
+	Point location;
+	Image picture;
+	public MockThing(Point loc){
+		location = loc;
+	}
+
+	@Override
+	public void setLocation(Point loc) {
+		location = loc;
+
+	}
+
+	@Override
+	public Point getLocation() {
+		return location;
+	}
+
+	@Override
+	public void setImage() {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+	@Override
+	public Image getImage() {
+		// TODO Auto-generated method stub
+		return picture;
+	}
+
+	@Override
+	public void setHP(int hp) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getHP() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void die() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setLayer(int layer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getLayer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void spawn(Point loc) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
