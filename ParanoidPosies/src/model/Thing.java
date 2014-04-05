ed200a253ea4a51993fed16c9a278872488129b0
Devin <Devin@10.142.183.196>
2014-04-04 20:25:21 -0700

    Thing interface

---
+++ ParanoidPosies/src/model/Thing.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.awt.Image;
+import java.awt.Point;
+
+public interface Thing {
+	public void setLocation(Point loc);
+	public Point getLocation();
+	public void setImage();
+	public Image getImage();
+	public void setHP(int hp);
+	public int getHP();
+	public void die();
+	public void setLayer(int layer);
+	public int getLayer();
+	public void spawn(Point loc);
+}
