1a0d6f7a8f58c9dac792b5c876630507b724eb22
dmb1 <dmb1@email.arizona.edu>
2014-04-06 09:56:33 -0700

    guardStrategy fix for going back to hive?

--- ParanoidPosies/src/model/GuardStrategy.java
+++ ParanoidPosies/src/model/GuardStrategy.java
@@ -4,7 +4,7 @@ import java.awt.Point;
 import java.util.Random;
 
 public class GuardStrategy implements BugStrategy {
-	private static final int RANDOM_CONST = 150;
+	private static final int RANDOM_CONST = 20;
 	private Bug bug;
 	private GameBoard board;
 	private Point rally;
@@ -18,20 +18,22 @@ public class GuardStrategy implements BugStrategy {
 	public void doNextAction() {
 
 		if (bug.getObjectivePoint() == null && bug.getObjectiveThing() != null) {
-			if (!bug.getLocation().equals(bug.getObjectiveThing().getLocation())) {
+			if (bug instanceof Bee && bug.getObjectiveThing() instanceof Hive) {
+				bug.setObjectiveToNull();
+				bug.setObjectivePoint(rally);
+			} 
+			else if (!bug.getLocation().equals(bug.getObjectiveThing().getLocation())) {
 				bug.move(bug.getObjectiveThing().getLocation());
-			} else if (bug.getLocation().equals(bug.getObjectiveThing().getLocation())
+			} 
+			else if (bug.getLocation().equals(bug.getObjectiveThing().getLocation())
 					&& bug.getObjectiveThing() instanceof Caterpillar
 					&& !bug.getObjectiveThing().isDead()) {
 				if (bug instanceof Bee) {
 					bug.attack(bug.getObjectiveThing());
 				}
-			} else if (bug instanceof Bee && bug.getObjectiveThing() instanceof Hive) {
-				if (bug instanceof Bee) {
-					bug.setObjectiveToNull();
-					bug.setObjectivePoint(rally);
-				}
-			} else if (bug.getLocation().equals(bug.getObjectiveThing().getLocation())
+			} 
+			
+			else if (bug.getLocation().equals(bug.getObjectiveThing().getLocation())
 					&& bug.getObjectiveThing().isDead()) {
 				bug.setObjectiveToNull();
 				bug.setObjectiveThing(bug.getClosestCaterpillar());
@@ -42,14 +44,16 @@ public class GuardStrategy implements BugStrategy {
 					bug.setObjectivePoint(rally);
 				}
 			}
-		} else if (bug.getObjectiveThing() == null && bug.getObjectivePoint() != null) {
+		} 
+		else if (bug.getObjectiveThing() == null && bug.getObjectivePoint() != null) {
 			Random r = new Random();
 			int x = bug.getObjectivePoint().x + r.nextInt(RANDOM_CONST) - RANDOM_CONST / 2;
 			int y = bug.getObjectivePoint().y + r.nextInt(RANDOM_CONST) - RANDOM_CONST / 2;
 			rally = new Point(x, y);
 			if (!bug.getLocation().equals(bug.getObjectivePoint())) {
 				bug.move(bug.getObjectivePoint());
-			} else if (bug.getLocation().equals(bug.getObjectivePoint()) && bug instanceof Bee) {
+			} 
+			else if (bug.getLocation().equals(bug.getObjectivePoint()) && bug instanceof Bee) {
 				bug.setObjectiveToNull();
 				bug.setObjectiveThing(bug.getClosestCaterpillar());
 				if (bug.getObjectiveThing() == null) {
