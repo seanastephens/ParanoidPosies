c85646b85b5601d3113055838a3a47b5a7dc0165
Sean Stephens <seanastephens@email.arizona.edu>
2014-04-05 19:04:46 -0700

    Reorganizing

--- ParanoidPosies/src/GUI/DenialPopup.java
+++ ParanoidPosies/src/GUI/DenialPopup.java
@@ -1,5 +1,69 @@
 package GUI;
 
-public class DenialPopup {
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import model.GameBoard;
+import model.Posie;
+
+public class DenialPopup extends JPanel {
+
+	private int WIDTH = 150; // default
+	private int HEIGHT = 80; // default
+
+	private GameBoard game;
+	private Point where;
+
+	public DenialPopup(GameBoard game, Point where) {
+		this.game = game;
+		this.where = where;
+		setSize(WIDTH, HEIGHT);
+		setBackground(Color.GRAY);
+		setLayout(null);
+
+		JButton yes = new JButton("Yes!");
+		yes.addActionListener(new YesListener());
+		add(yes);
+		yes.setLocation(new Point(0, HEIGHT / 2));
+		yes.setSize(WIDTH / 2, HEIGHT / 2);
+
+		JButton no = new JButton("no.");
+		no.addActionListener(new NoListener());
+		add(no);
+		no.setLocation(WIDTH / 2, HEIGHT / 2);
+		no.setSize(WIDTH / 2, HEIGHT / 2);
+
+		JLabel q = new JLabel("<html>Put a plant here?</html>");
+		add(q);
+		q.setLocation(0, 0);
+		q.setSize(WIDTH, HEIGHT / 2);
+	}
+
+	private class YesListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			game.getAllThingsOnBoard().add(new Posie(where));
+			System.out.println(game.getAllThingsOnBoard());
+			setVisible(false);
+			setEnabled(false);
+			((GamePanel) getParent()).popup = null;
+			getParent().repaint();
+		}
+	}
+
+	private class NoListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			setVisible(false);
+			setEnabled(false);
+			((GamePanel) getParent()).popup = null;
+		}
+
+	}
 }
--- ParanoidPosies/src/model/GameBoard.java
+++ ParanoidPosies/src/model/GameBoard.java
@@ -19,6 +19,9 @@ public class GameBoard implements GameInterface {
 	public static final int INCREASE_ENEMIES_BY = 2;
 	public static final int GRACE_PERIOD = 30 * PPGUI.UPDATES_PER_SEC;
 
+	public static final int POSIE_COST_IN_HONEY = 2;
+	public static final int POSIE_COST_IN_SEEDS = 1;
+
 	public static final int BEE_SPAWN_X_OFFSET = 100;
 	public static final int BEE_SPAWN_Y_OFFSET = 100;
 
@@ -26,17 +29,16 @@ public class GameBoard implements GameInterface {
 	private int timer;
 	private List<Bug> enemyList;
 	private List<Bug> friendlyList;
-	
+
 	public static final Point NORTH_SPAWN = new Point(centerX, centerY - 700);
 	public static final Point EAST_SPAWN = new Point(centerX + 700, centerY);
 	public static final Point SOUTH_SPAWN = new Point(centerX, centerY + 700);
 	public static final Point WEST_SPAWN = new Point(centerX - 700, centerY);
 	public static final int SPAWN_PROBABILITY = 1;
-	
+
 	public static final int ENEMY_DISTANCE_FROM_HIVE = 600;
 	public static final int ENEMY_NORTH_SPAWN_RANGE = 100;
 	public static final int ENEMY_EAST_SPAWN_RANGE = 100;
-	
 
 	public GameBoard() {
 		timer = 0;
@@ -47,22 +49,22 @@ public class GameBoard implements GameInterface {
 		enemyList = new ArrayList<Bug>();
 		things.add(hive);
 		for (int i = 0; i < STARTING_BEES; i++) {
-			
+
 			Bee b = new Bee(getRandomBeeSpawn(), this);
 			things.add(b);
 			friendlyList.add(b);
 		}
 
 		// These Things are just here for testing
-		//things.add(new Caterpillar(new Point(2200, 2200), this));
+		// things.add(new Caterpillar(new Point(2200, 2200), this));
 		things.add(new Posie(new Point(centerX, centerY + 200)));
 
 		((Bee) things.get(1)).setStrategy(new GatherStrategy(((Bug) things.get(1)), this),
 				things.get(6));
 
 	}
-	
-	public Point getRandomBeeSpawn(){
+
+	public Point getRandomBeeSpawn() {
 		Random beeSpawn = new Random();
 		int randX = beeSpawn.nextInt(BEE_SPAWN_X_OFFSET) + centerX;
 		int randY = beeSpawn.nextInt(BEE_SPAWN_Y_OFFSET) + centerY;
@@ -118,78 +120,85 @@ public class GameBoard implements GameInterface {
 		}
 		for (Thing t : toRemove) {
 			things.remove(t);
-		}		
+		}
 		spawnEnemies();
 	}
-	
-	public void spawnEnemies(){
-		for(int i = 0; i < getNumberOfEnemiesToSpawn(); i++){
+
+	public void spawnEnemies() {
+		for (int i = 0; i < getNumberOfEnemiesToSpawn(); i++) {
 			Point toSpawnAt = null;
 			Random rand = new Random();
-			int randInt = rand.nextInt(4) + 1; //Get random integer from 1-4
-			switch(randInt){
-			case 1: toSpawnAt = getNorthSpawn(); break;
-			case 2: toSpawnAt = getEastSpawn(); break;
-			case 3: toSpawnAt = getSouthSpawn(); break;
-			case 4: toSpawnAt = getWestSpawn(); break;
-			} 
+			int randInt = rand.nextInt(4) + 1; // Get random integer from 1-4
+			switch (randInt) {
+			case 1:
+				toSpawnAt = getNorthSpawn();
+				break;
+			case 2:
+				toSpawnAt = getEastSpawn();
+				break;
+			case 3:
+				toSpawnAt = getSouthSpawn();
+				break;
+			case 4:
+				toSpawnAt = getWestSpawn();
+				break;
+			}
 			int spawnProb = rand.nextInt(100);
-			if(spawnProb < SPAWN_PROBABILITY){
+			if (spawnProb < SPAWN_PROBABILITY) {
 				Caterpillar c = new Caterpillar(toSpawnAt, this);
 				things.add(c);
 				enemyList.add(c);
 			}
 		}
 	}
-	
-	public Point getWestSpawn(){
+
+	public Point getWestSpawn() {
 		Random rand = new Random();
 		int randX = centerX - (rand.nextInt(ENEMY_EAST_SPAWN_RANGE) + ENEMY_DISTANCE_FROM_HIVE);
-		
-		int randY = rand.nextInt(ENEMY_NORTH_SPAWN_RANGE) - (ENEMY_NORTH_SPAWN_RANGE/2);
+
+		int randY = rand.nextInt(ENEMY_NORTH_SPAWN_RANGE) - (ENEMY_NORTH_SPAWN_RANGE / 2);
 		randY += centerY;
-		
+
 		return new Point(randX, randY);
 	}
-	
-	public Point getSouthSpawn(){
+
+	public Point getSouthSpawn() {
 		Random rand = new Random();
-		int randX = rand.nextInt(ENEMY_EAST_SPAWN_RANGE) - (ENEMY_EAST_SPAWN_RANGE/2);
+		int randX = rand.nextInt(ENEMY_EAST_SPAWN_RANGE) - (ENEMY_EAST_SPAWN_RANGE / 2);
 		randX += centerX;
-		
+
 		int randY = rand.nextInt(ENEMY_NORTH_SPAWN_RANGE) + ENEMY_DISTANCE_FROM_HIVE + centerY;
-		
+
 		return new Point(randX, randY);
 	}
-	
-	public Point getEastSpawn(){
+
+	public Point getEastSpawn() {
 		Random rand = new Random();
 		int randX = centerX + (rand.nextInt(ENEMY_EAST_SPAWN_RANGE) + ENEMY_DISTANCE_FROM_HIVE);
-		
-		int randY = rand.nextInt(ENEMY_NORTH_SPAWN_RANGE) - ENEMY_NORTH_SPAWN_RANGE/2;
+
+		int randY = rand.nextInt(ENEMY_NORTH_SPAWN_RANGE) - ENEMY_NORTH_SPAWN_RANGE / 2;
 		randY += centerY;
-		
+
 		return new Point(randX, randY);
 	}
-	
-	public Point getNorthSpawn(){
+
+	public Point getNorthSpawn() {
 		Random rand = new Random();
-		int randX = rand.nextInt(ENEMY_EAST_SPAWN_RANGE) - ENEMY_EAST_SPAWN_RANGE/2;
+		int randX = rand.nextInt(ENEMY_EAST_SPAWN_RANGE) - ENEMY_EAST_SPAWN_RANGE / 2;
 		randX += centerX;
-		
+
 		int randY = centerY - (rand.nextInt(ENEMY_NORTH_SPAWN_RANGE) + ENEMY_DISTANCE_FROM_HIVE);
-		
+
 		return new Point(randX, randY);
 	}
-	
-	
-	
-	public int getNumberOfEnemiesToSpawn(){
-		if(timer < GRACE_PERIOD){
+
+	public int getNumberOfEnemiesToSpawn() {
+		if (timer < GRACE_PERIOD) {
 			return 0;
-		}
-		else return (INITIAL_ENEMY_THRESHOLD + INCREASE_ENEMIES_BY * (timer/ DIFFICULTY_INTERVAL)) - enemyList.size();
-		
+		} else
+			return (INITIAL_ENEMY_THRESHOLD + INCREASE_ENEMIES_BY * (timer / DIFFICULTY_INTERVAL))
+					- enemyList.size();
+
 	}
 
 }
