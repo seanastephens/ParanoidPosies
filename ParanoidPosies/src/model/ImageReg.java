87619210f0506365075d4a4107e1600e48e0d9ca
kuba1 <kubastechly@gmail.com>
2014-04-05 17:34:55 -0700

    Changed Caterpillar to have alpha background.

--- ParanoidPosies/src/model/ImageReg.java
+++ ParanoidPosies/src/model/ImageReg.java
@@ -22,7 +22,7 @@ public class ImageReg {
 		images.put("Bee", getImage("bee.png"));
 		images.put("Hive", getImage("beehive.png").getScaledInstance(200, 200, 0));
 		images.put("GrassTile", getImage("grasstile.png"));
-		images.put("Caterpillar", getImage("caterpillar.jpg"));
+		images.put("Caterpillar", getImage("caterpillar.png"));
 		images.put("HITLER", getImage("HITLER.gif"));
 		
 		//Filler images
