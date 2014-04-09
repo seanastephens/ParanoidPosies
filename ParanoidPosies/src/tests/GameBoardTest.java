6564e732657d07708336d3175e771579e0f8d625
Sean Stephens <seanastephens@email.arizona.edu>
2014-04-09 12:20:14 -0700

    Added GameBoardTest
    Currently, this method only tests the getClosest and getRandom methods.
    
    In the future, I am going to move the getClosest/Random methods out of
    Bug, and replace them with these. That way they are in only one place
    and can be optimized.
    
    Additionally, I am going to make the Map a Singleton -- that way, we can
    stop passing it around as a parameter everywhere.

---
+++ ParanoidPosies/src/tests/GameBoardTest.java
@@ -0,0 +1,60 @@
+package tests;
+
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Point;
+import java.util.List;
+
+import model.Bee;
+import model.Caterpillar;
+import model.GameBoard;
+import model.Posie;
+import model.Thing;
+
+import org.junit.Test;
+
+public class GameBoardTest {
+
+	Point origin = new Point(0, 0);
+
+	@Test
+	public void getClosestTest() {
+		GameBoard g = new GameBoard(false);
+
+		Thing t = g.getClosest(Posie.class, origin);
+		assertTrue(t instanceof Posie);
+		t = g.getClosest(Bee.class, origin);
+		assertTrue(t instanceof Bee);
+		t = g.getClosest(Caterpillar.class, origin);
+		assertTrue(t == null);
+	}
+
+	@Test
+	public void getClosestReturnsClosest() {
+		GameBoard g = new GameBoard(false);
+
+		Thing closest = g.getClosest(Posie.class, origin);
+		assertTrue(closest instanceof Posie);
+
+		List<Thing> allThings = g.getAllThingsOnBoard();
+		for (Thing t : allThings) {
+			if (t instanceof Posie) {
+				assertTrue(origin.distance(t.getLocation()) >= origin.distance(closest
+						.getLocation()));
+			}
+		}
+	}
+
+	@Test
+	public void getRandomTest() {
+		GameBoard g = new GameBoard(false);
+
+		Thing t = g.getRandom(Posie.class);
+		assertTrue(t instanceof Posie);
+		t = g.getRandom(Bee.class);
+		assertTrue(t instanceof Bee);
+		t = g.getRandom(Caterpillar.class);
+		assertTrue(t == null);
+	}
+
+}
